5a168c2c0906d159917439e7e60b358f651573a8
pds17100 <pds17100@naver.com>
2024-03-30 16:09:33 +0900

    기하

---
+++ src/기하_직사각형과삼각형/수학은체육과목입니다.java
@@ -0,0 +1,16 @@
+package 기하_직사각형과삼각형;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 수학은체육과목입니다 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Integer.parseInt(br.readLine());
+        long cm = N*4;
+        System.out.println(cm);
+        br.close();
+
+    }
+}
