72e88ef9d3421e9d8ad2cd6fde55346ce332fc60
pds17100 <pds17100@naver.com>
2024-04-03 20:18:56 +0900

    브루트포스

---
+++ src/브루트포스/블랙잭.java
@@ -0,0 +1,41 @@
+package 브루트포스;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class 블랙잭 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        System.out.println(BlackJack(arr,N,M));
+    }
+
+    public static int BlackJack(int arr[], int N, int M) {
+        int result = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int num = arr[i] + arr[j] + arr[k];
+                    if (num < M) {
+                        result = Math.max(result, num);
+                    } else if (num == M) {
+                        return M;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
