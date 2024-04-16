4042e3415068105881d590290ab1790fa23b3672
pds17100 <pds17100@naver.com>
2024-04-16 15:47:34 +0900

    맵 1

---
+++ src/집합과맵/숫자카드.java
@@ -0,0 +1,34 @@
+package 집합과맵;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+/**
+ * Hash map
+ */
+public class 숫자카드 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        HashMap<String, Integer> hashMap = new HashMap<>();
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            hashMap.put(st.nextToken(), 0);
+        }
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (hashMap.get(st.nextToken()) != null) {
+                sb.append("1 ");
+            } else {
+                sb.append("0 ");
+            }
+        }
+        System.out.print(sb.toString());
+    }
+}
