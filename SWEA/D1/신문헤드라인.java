a71fdf77a5407366494c2099da944eb945599924
pds17100 <pds17100@naver.com>
2024-04-29 23:14:56 +0900

    swea 1

---
+++ SWEA/D1/신문헤드라인.java
@@ -0,0 +1,11 @@
+package D1;
+
+import java.util.Scanner;
+
+public class 신문헤드라인 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println(str.toUpperCase());
+    }
+}
