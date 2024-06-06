8e686a5ed293ca1184dd2354bb839bf0c1ea25b7
pds17100 <pds17100@naver.com>
2024-06-06 21:52:17 +0900

    prgrammers start

---
+++ prgrammers/level0/몫구하기.java
@@ -0,0 +1,12 @@
+package prgrammers.level0;
+
+class 몫구하기 {
+    public static void main(String[] args) {
+        System.out.println(solution(10, 5));
+    }
+    public static int solution(int num1, int num2) {
+        int answer = 0;
+        answer = num1/num2;
+        return answer;
+    }
+}
