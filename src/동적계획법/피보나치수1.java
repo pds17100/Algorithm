add57a0780c50f597668e186a970c6437070cf49
pds17100 <pds17100@naver.com>
2024-10-03 16:56:56 +0900

    백준 1

---
+++ src/동적계획법/피보나치수1.java
@@ -0,0 +1,41 @@
+package 동적계획법;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 피보나치수1 {
+    static int code1Cnt, code2Cnt;
+    static int f[];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        f = new int[n];
+        code1Cnt=0;
+        code2Cnt = 0;
+
+        fib(n);
+        fibonacci(n);
+        System.out.println(code1Cnt + " " + code2Cnt);
+    }
+
+    static int fib(int n) {
+        if (n == 1 || n == 2) {
+            code1Cnt++;
+            return 1;
+        } else {
+            return (fib(n - 1) + fib(n - 2));
+        }
+    }
+
+    static int fibonacci(int n){
+        f[0] = 1;
+        f[1] = 1;
+
+        for(int i = 2; i < n; i++){
+            code2Cnt++;
+            f[i] = f[i-1] + f[i-2];
+        }
+        return f[n-1];
+    }
+}
