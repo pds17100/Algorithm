c8b3a16f7b0ad803dc93020feaca57fcec8c304b
pds17100 <92380088+pds17100@users.noreply.github.com>
2024-12-16 15:26:31 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 928 ms, Memory: 93820 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,64 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static ArrayList<Integer> A[];
+    static int result[];
+    static boolean visited[];
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        A = new ArrayList[N + 1];
+        result = new int[N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            A[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            A[start].add(end);
+            A[end].add(start);
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            Collections.sort(A[i]);
+        }
+        BFS(R);
+
+        for (int i = 1; i < N + 1; i++) {
+            sb.append(result[i]).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    private static void BFS(int R) {
+        int cnt = 1;
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(R);
+        visited[R] = true;
+        result[R] = cnt++;
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int i : A[cur]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    result[i] = cnt++;
+                    queue.offer(i);
+                }
+            }
+        }
+    }
+
+}
