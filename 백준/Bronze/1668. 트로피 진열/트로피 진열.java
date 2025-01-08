0720b1a9c7ddfd5495943210e7a5522c5dfd01db
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-01-08 22:08:06 +0900

    [Bronze II] Title: 트로피 진열, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/1668. 트로피 진열/트로피 진열.java
+++ 백준/Bronze/1668. 트로피 진열/트로피 진열.java
@@ -9,20 +9,23 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = scan.nextInt();
         }
+        
         int cnt1 = 1;
+        int max_left = arr[0];
         for (int i = 1; i < N; i++) {
-            if (arr[0] < arr[i]) {
+            if (max_left < arr[i]) {
                 cnt1++;
-                arr[0] = arr[i];
+                max_left = arr[i];
             }
         }
         System.out.println(cnt1);
-
+        
         int cnt2 = 1;
+        int max_right = arr[N - 1];
         for (int i = N - 1; i > 0; i--) {
-            if (arr[N - 1] < arr[i - 1]) {
+            if (max_right < arr[i - 1]) {
                 cnt2++;
-                arr[N - 1] = arr[i - 1];
+                max_right = arr[i - 1];
             }
         }
         System.out.println(cnt2);
