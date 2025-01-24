d1575dcfae83ae6bc5b58d194f63e45811ab45ad
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-01-24 15:14:24 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 872 ms, Memory: 104304 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.java
@@ -0,0 +1,77 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    static boolean visited[];
+    static int result[];
+    static ArrayList<Integer> list[];
+    static int cnt = 1;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
+
+        list = new ArrayList[N + 1];
+        visited = new boolean[N + 1];
+        result = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            list[i] = new ArrayList<>();
+        }
+
+        for (int i = 1; i <= M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            list[start].add(end);
+            list[end].add(start);
+        }
+
+/*        for (int i = 1; i <= N; i++) {
+            Collections.sort(list[i], new Comparator<Integer>() {
+                @Override
+                public int compare(Integer o1, Integer o2) {
+                    return o2 - o1;
+                }
+            });
+        }*/
+
+/*        for (int i = 1; i <= N; i++) {
+            Collections.sort(list[i]);
+        }*/
+
+        if (!visited[R]) {
+            dfs(R);
+        }
+
+        for (int i = 1; i <= N; i++) {
+            sb.append(result[i]).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+
+    private static void dfs(int R) {
+        if (visited[R]) {
+            return;
+        }
+        visited[R] = true;
+        result[R] = cnt++;
+        Collections.sort(list[R], Collections.reverseOrder());
+        for (Integer i : list[R]) {
+            if (!visited[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+}
