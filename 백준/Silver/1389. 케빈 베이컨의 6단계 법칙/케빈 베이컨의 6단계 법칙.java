c6ca05af01f49d4108e04e3690a2577d38d9e231
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-02-02 14:26:38 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 124 ms, Memory: 14728 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.java
@@ -0,0 +1,56 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static int arr[][];
+    static final int INF = 987654321;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1][N + 1];
+        for (int i = 1; i <= N; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = INF;
+                if (i == j) {
+                    arr[i][j] = 0;
+                }
+            }
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            arr[a][b] = 1;
+            arr[b][a] = 1;
+        }
+
+        for (int k = 1; k <= N; k++) {
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= N; j++) {
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
+                }
+            }
+        }
+
+        int tmp = INF;
+        int result = 0;
+        for (int i = 1; i <= N; i++) {
+            int total = 0;
+            for (int j = 1; j <= N; j++) {
+                total += arr[i][j];
+            }
+            if (tmp > total) {
+                tmp = total;
+                result = i;
+            }
+        }
+        System.out.println(result);
+    }
+}
