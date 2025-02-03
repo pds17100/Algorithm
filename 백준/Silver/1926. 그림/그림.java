22fd0b834c1368dceb5b1796caf937a02e6aa3f3
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-02-03 21:26:58 +0900

    [Silver I] Title: 그림, Time: 344 ms, Memory: 53556 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.java
@@ -0,0 +1,57 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int map[][];
+    static boolean visited[][];
+    static int maxSize = 0;
+    static int cnt = 0;
+    static int dx[] = {0, 1, -1, 0};
+    static int dy[] = {1, 0, 0, -1};
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 1 && !visited[i][j]) {
+                    int size = dfs(i, j);
+                    maxSize = Math.max(maxSize, size);
+                    cnt++;
+                }
+            }
+        }
+
+        System.out.println(cnt);
+        System.out.println(maxSize);
+    }
+
+    private static int dfs(int x, int y) {
+        visited[x][y] = true;
+        int size = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && map[nx][ny] == 1) {
+                size += dfs(nx, ny);
+            }
+        }
+        return size;
+    }
+}
