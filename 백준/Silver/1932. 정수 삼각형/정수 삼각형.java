63a9e2319f9a31f2d26bbd67e9048ffd57870928
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-02-18 17:16:21 +0900

    [Silver I] Title: 정수 삼각형, Time: 260 ms, Memory: 28604 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -25,15 +25,25 @@ public class Main {
             dp[N - 1][i] = arr[N - 1][i];
         }
 
-        System.out.println(find(0, 0));
-    }
-
-    static int find(int depth, int index) {
-        if (depth == N - 1) return dp[depth][index];
-
-        if (dp[depth][index] == null) {
-            dp[depth][index] = Math.max(find(depth + 1, index), find(depth + 1, index + 1)) + arr[depth][index];
+        for (int i = N - 2; i >= 0; i--) {  //바텀업
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.max(dp[i + 1][j], dp[i + 1][j + 1]) + arr[i][j];
+            }
         }
-        return dp[depth][index];
+        System.out.println(dp[0][0]);
+
+//        find(0, 0);
     }
+
+    private static int find(int depth, int idx) {   //탑다운
+        if (depth == N - 1) {
+            return dp[depth][idx];
+        }
+
+        if (dp[depth][idx] == null) {
+            dp[depth][idx] = Math.max(find(depth + 1, idx), find(depth + 1, idx + 1)) + arr[depth][idx];
+        }
+        return dp[depth][idx];
+    }
+
 }
