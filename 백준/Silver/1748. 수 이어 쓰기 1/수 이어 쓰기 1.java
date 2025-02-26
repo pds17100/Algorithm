39b80e97c45bd845a3c6e3493ee8fbb1e8e1419d
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-02-26 21:15:14 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 456 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.java
@@ -0,0 +1,22 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+        int plus = 1;
+        int num = 10;
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            if (i % num == 0) {
+                plus++;
+                num *= 10;
+            }
+            result += plus;
+        }
+        System.out.println(result);
+    }
+}
