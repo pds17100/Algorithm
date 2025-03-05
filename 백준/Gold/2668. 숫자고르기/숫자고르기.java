c23c2c652dd52d072bd08b75e3fa08f25b22717d
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-03-05 22:32:32 +0900

    [Gold V] Title: 숫자고르기, Time: 100 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.java
@@ -0,0 +1,53 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    static int N;
+    static int arr[];
+    static boolean visited[];
+    static ArrayList<Integer> list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N + 1];
+        visited = new boolean[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        list = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            visited[i] = true;
+            dfs(i, i);
+            visited[i] = false;
+        }
+
+        Collections.sort(list);
+        System.out.println(list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i));
+        }
+
+    }
+
+    private static void dfs(int start, int target) {
+        if (!visited[arr[start]]) {
+            visited[arr[start]] = true;
+            dfs(arr[start], target);
+            visited[arr[start]] = false;
+        }
+
+        if (arr[start] == target) {
+            list.add(target);
+        }
+
+    }
+}
