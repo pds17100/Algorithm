b6d4012f14194ccd81870bb6a9525c557971b1f6
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-03-13 00:34:59 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 112 ms, Memory: 15304 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,74 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N, M;
+    static char[][] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new char[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = str.charAt(j);
+            }
+        }
+
+        int minCount = Integer.MAX_VALUE;
+
+        for (int i = 0; i <= N - 8; i++) {
+            for (int j = 0; j <= M - 8; j++) {
+                minCount = Math.min(minCount, getRepaintCount(i, j));
+            }
+        }
+
+        System.out.println(minCount);
+    }
+
+    static int getRepaintCount(int startX, int startY) {
+        int count1 = 0;
+        int count2 = 0;
+        char[][] chess1 = {
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'}
+        };
+        char[][] chess2 = {
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'},
+                {'B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'},
+                {'W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'}
+        };
+
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                if (arr[startX + i][startY + j] != chess1[i][j]) {
+                    count1++;
+                }
+                if (arr[startX + i][startY + j] != chess2[i][j]) {
+                    count2++;
+                }
+            }
+        }
+
+        return Math.min(count1, count2);
+    }
+}
