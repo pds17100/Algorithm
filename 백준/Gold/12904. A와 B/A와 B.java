cf3d49046ae099af78db14e6514e7500f94a1b4f
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-03-24 20:12:11 +0900

    [Gold V] Title: A와 B, Time: 108 ms, Memory: 15788 KB -BaekjoonHub

---
+++ 백준/Gold/12904. A와 B/A와 B.java
@@ -0,0 +1,21 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        String T = br.readLine();
+
+        while (T.length()>S.length()) {
+            if (T.charAt(T.length() - 1) == 'A') {
+                T = T.substring(0, T.length() - 1);
+            } else {
+                T = new StringBuilder(T.substring(0, T.length() - 1)).reverse().toString();
+            }
+        }
+        System.out.println(T.equals(S) ? 1 : 0);
+    }
+}
