b0d26ef3169923b967341479d2b48399d376410a
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-04-04 21:49:20 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -2,47 +2,52 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
+    static int N, M;
+    static ArrayList<Integer> list[];
     static boolean visited[];
-    static ArrayList<Integer> A[];
-    static int cnt = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int M = Integer.parseInt(br.readLine());
-        A = new ArrayList[N + 1];
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        list = new ArrayList[N + 1];
         visited = new boolean[N + 1];
 
         for (int i = 1; i <= N; i++) {
-            A[i] = new ArrayList<>();
+            list[i] = new ArrayList<>();
         }
 
-        for (int i = 1; i <= M; i++) {
+        for (int i = 0; i < M; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            A[start].add(end);
-            A[end].add(start);
+            int s = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            list[s].add(e);
+            list[e].add(s);
         }
 
-        dfs(1);
+        int result = bfs(1);
+        System.out.println(result);
 
-        System.out.println(cnt);
     }
 
-    private static void dfs(int R) {
-        if (visited[R]) {
-            return;
-        }
+    private static int bfs(int R) {
+        Queue<Integer> q = new LinkedList<>();
         visited[R] = true;
-        for (int i : A[R]) {
-            if (!visited[i]) {
-                cnt++;
-                dfs(i);
+        q.offer(R);
+        int cnt = 0;
+
+        while (!q.isEmpty()) {
+            int now = q.poll();
+            for (int i : list[now]) {
+                if (!visited[i]) {
+                    visited[i] = true;
+                    q.offer(i);
+                    cnt++;
+                }
             }
         }
+        return cnt;
     }
 }
