e984479f1c8390b5370fe9d48bd87b9b405e544d
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-04-27 23:34:50 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17580 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner in = new Scanner(System.in);
+		int arr[] = new int[9];
+		int max= 0;
+		int index = 0;
+		
+		for(int i=0; i<9; i++) {
+			arr[i] = in.nextInt();
+		}
+		in.close();
+	
+		for(int i =0; i<arr.length; i++) {	
+			if(arr[i]>max) {
+				max = arr[i];
+				index = i+1;
+			}
+		}
+		System.out.println(max);
+		System.out.println(index);
+	}
+}
