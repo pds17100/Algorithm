7ae537b743b917bdb53a9610338c18751bc794bf
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-05-01 23:44:11 +0900

    [Silver I] Title: 절댓값 힙, Time: 284 ms, Memory: 25756 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,40 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                if (Math.abs(o1) > Math.abs(o2)) {
+                    return Math.abs(o1) - Math.abs(o2);
+                } else if (Math.abs(o1) == Math.abs(o2)) {
+                    return o1 - o2;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                pq.offer(num);
+            } else {
+                if (pq.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(pq.poll()).append('\n');
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
