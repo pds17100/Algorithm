4e4485ed50edb8bda81691f115431bec21f985f8
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-05-01 23:26:30 +0900

    [Silver II] Title: 최소 힙, Time: 280 ms, Memory: 26364 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,27 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                pq.offer(num);
+            } else {
+                if (pq.isEmpty()) {
+                    sb.append(0).append('\n');
+                } else {
+                    sb.append(pq.poll()).append('\n');
+                }
+            }
+        }
+        System.out.println(sb);
+    }
+}
