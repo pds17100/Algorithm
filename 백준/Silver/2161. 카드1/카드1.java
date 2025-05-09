62b4efc9ae8cdb23a9c4d12b03cd4b15b8da1a7c
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-05-09 23:30:35 +0900

    [Silver V] Title: 카드1, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= N; i++) {
+            q.offer(i);
+        }
+        while(q.size()>1) {
+            sb.append(q.poll()).append(" ");
+            q.offer(q.poll());
+        }
+        sb.append(q.poll());
+        System.out.println(sb);
+    }
+}
