5e8190d0a57f0492681f4ec989268dae28c24988
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-05-21 16:18:46 +0900

    [D4] Title: [S/W 문제해결 기본] 7일차 - 미로1, Time: 92 ms, Memory: 22,528 KB -BaekjoonHub

--- SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
+++ SWEA/D4/1226. ［S／W 문제해결 기본］ 7일차 － 미로1/［S／W 문제해결 기본］ 7일차 － 미로1.java
@@ -2,13 +2,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
     static int arr[][];
     static boolean visited[][];
     static int dx[] = {0, 1, -1, 0};
     static int dy[] = {1, 0, 0, -1};
-    static boolean found;
+    static int startX, startY;
+    static int result;
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,9 +19,7 @@ public class Solution {
             int caseNum = Integer.parseInt(br.readLine());
             arr = new int[16][16];
             visited = new boolean[16][16];
-            found = false;
-            int startX = 0;
-            int startY = 0;
+            result = 0;
             for (int i = 0; i < 16; i++) {
                 String str = br.readLine();
                 for (int j = 0; j < 16; j++) {
@@ -32,31 +33,43 @@ public class Solution {
                     }
                 }
             }
-            dfs(startX, startY);
-            sb.append("#").append(t).append(" ").append(found ? 1 : 0).append("\n");
+            bfs(startX, startY);
+            sb.append("#").append(t).append(" ").append(result).append("\n");
         }
-
         System.out.println(sb);
     }
 
-    private static void dfs(int x, int y) {
-        if (found) {
-            return;
-        }
+    private static void bfs(int x, int y) {
+        Queue<Point> q = new LinkedList<>();
+        q.offer(new Point(x, y));
         visited[x][y] = true;
 
-        for (int i = 0; i < 4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < 16 && ny >= 0 && ny < 16 && !visited[nx][ny]) {
-                if (arr[nx][ny] == 3) {
-                    found = true;
-                    return;
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nx = p.x + dx[i];
+                    int ny = p.y + dy[i];
+                    if (nx >= 0 && nx < 16 && ny >= 0 && ny < 16 && arr[nx][ny] != 1 && !visited[nx][ny]) {
+                        if (arr[nx][ny] == 3) {
+                            result = 1;
+                            return;
+                        } else {
+                            visited[nx][ny] = true;
+                            q.offer(new Point(nx, ny));
+                        }
+                    }
                 }
-                if (arr[nx][ny] == 0) {
-                    dfs(nx, ny);
-                }
-            }
+        }
+
+    }
+
+    static class Point {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
