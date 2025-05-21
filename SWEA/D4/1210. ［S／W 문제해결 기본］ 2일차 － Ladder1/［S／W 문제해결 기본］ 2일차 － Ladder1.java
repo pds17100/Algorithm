d99599381bf4bf3a3f1bbd572b4d9546c64539c3
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-05-21 17:21:03 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 150 ms, Memory: 34,944 KB -BaekjoonHub

--- SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -6,52 +6,48 @@ import java.util.StringTokenizer;
 
 public class Solution {
     static int arr[][];
-    static boolean visited[][];
-    static int dx[] = {0, 0, -1};
-    static int dy[] = {-1, 1, 0};
+    static int arriveX, arriveY;
     static int startY;
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         for (int t = 1; t <= 10; t++) {
             int caseNum = Integer.parseInt(br.readLine());
             arr = new int[100][100];
-            visited = new boolean[100][100];
-
-            int endX = 0;
-            int endY = 0;
-
+            arriveX = 0;
+            arriveY = 0;
+            startY = 0;
             for (int i = 0; i < 100; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine(), " ");
                 for (int j = 0; j < 100; j++) {
                     arr[i][j] = Integer.parseInt(st.nextToken());
                     if (arr[i][j] == 2) {
-                        endX = i;
-                        endY = j;
+                        arriveX = i;
+                        arriveY = j;
                     }
                 }
             }
-
-            dfs(endX, endY);
+            trace(arriveX, arriveY);
             sb.append("#").append(t).append(" ").append(startY).append("\n");
+
         }
         System.out.println(sb);
     }
 
-    private static void dfs(int x, int y) {
-        visited[x][y] = true;
-        if (x == 0) {
-            startY = y;
-            return;
-        }
-
-        for (int i = 0; i < 3; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            if (nx >= 0 && nx < 100 && ny >= 0 && ny < 100 && arr[nx][ny] == 1 && !visited[nx][ny]) {
-                dfs(nx, ny);
-                return;
+    private static void trace(int x, int y) {
+        while (x > 0) {
+            if (y > 0 && arr[x][y - 1] == 1) {
+                while (y > 0 && arr[x][y - 1] == 1) {
+                    y--;
+                }
+            } else if (y < 99 && arr[x][y + 1] == 1) {
+                while (y < 99 && arr[x][y + 1] == 1) {
+                    y++;
+                }
             }
+            x--;
         }
+        startY = y;
     }
 }
