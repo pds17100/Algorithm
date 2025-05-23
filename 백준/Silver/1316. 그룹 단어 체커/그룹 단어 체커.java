01db2e37a46f4dd957246ae610927640737da448
pds17100 <92380088+pds17100@users.noreply.github.com>
2025-05-23 19:17:06 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,40 +1,30 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args) throws IOException {
-        int N = Integer.parseInt(br.readLine());
-        int count = 0;
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int result = 0;
+		for(int i=0; i<N; i++) {
+			HashSet<Character> set = new HashSet<>();
+			boolean check = true;
+			String str = br.readLine();
+			set.add(str.charAt(0));
+			for(int j=1; j<str.length(); j++) {
+				if(set.contains(str.charAt(j)) && str.charAt(j) != str.charAt(j-1)) {
+					check = false;
+					break;
+				} else {
+					set.add(str.charAt(j));
+				}
+			}
+			if(check) result++;
+		}
+		System.out.println(result);
+	}
 
-        for(int i=0; i<N; i++) {
-            if(check() == true) {
-                count++;
-            }
-        }
-        System.out.println(count);
-    }
-
-    private static boolean check() throws IOException {
-        boolean[] check = new boolean[26];
-        int prev = 0;
-        String str = br.readLine();
-        for(int i=0; i<str.length(); i++) {
-            int now = str.charAt(i);
-            if(prev != now) {
-                if(check[now - 'a'] == false) {
-                    check[now - 'a'] = true;
-                    prev = now;
-                }
-                else{
-                    return false;
-                }
-            }
-            else{
-                continue;
-            }
-        }
-        return true;
-    }
 }
