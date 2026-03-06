831774bb1701632fae62a7536a581a1591be1c05
pds17100 <92380088+pds17100@users.noreply.github.com>
2026-03-06 20:07:05 +0900

    [Silver IV] Title: 학생 번호, Time: 128 ms, Memory: 16784 KB -BaekjoonHub

---
+++ 백준/Silver/1235. 학생 번호/학생 번호.java
@@ -0,0 +1,35 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String str[] = new String[N];
+        for (int i = 0; i < N; i++) {
+            str[i] = br.readLine();
+        }
+        int len = str[0].length();
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 1; i <= len; i++) {
+            set.clear();
+            for (int j = 0; j < N; j++) {
+                String code = str[j].substring(len - i, len);
+                if (set.contains(code)) {
+                    break;
+                } else {
+                    set.add(code);
+                }
+            }
+            if (set.size() == N) {
+                System.out.println(i);
+                return;
+            }
+        }
+
+    }
+}
