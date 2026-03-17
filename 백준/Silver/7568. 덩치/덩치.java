2fb2dc7014abbed3cb29ae99aca9e25ae4a2e62e
pds17100 <92380088+pds17100@users.noreply.github.com>
2026-03-17 20:49:30 +0900

    [Silver V] Title: 덩치, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/7568. 덩치/덩치.java
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -9,21 +9,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int arr[][] = new int[N][2];
+
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
+
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             int rank = 1;
             for (int j = 0; j < N; j++) {
-                if(i==j) continue;
+                if (i == j) continue;
                 if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) {
                     rank++;
                 }
             }
-            System.out.print(rank + " ");
+            sb.append(rank).append(' ');
         }
+
+        System.out.println(sb);
+
     }
+
 }
